769a99c0799673534bd888e02734d30e297d4ad5
James Ward <james@jamesward.org>
2016-01-07 14:11:35 -0700

    init

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var nforce = require('nforce');
+var hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.set('trust proxy', 'loopback');
+
+function isSetup() {
+  return (process.env.CONSUMER_KEY != null) && (process.env.CONSUMER_SECRET != null);
+}
+
+function oauthCallbackUrl(req) {
+  return req.protocol + '://' + req.get('host');
+}
+
+hbs.registerHelper('get', function(field) {
+  return this.get(field);
+});
+
+app.get('/', function(req, res) {
+  if (isSetup()) {
+    var org = nforce.createConnection({
+      clientId: process.env.CONSUMER_KEY,
+      clientSecret: process.env.CONSUMER_SECRET,
+      redirectUri: oauthCallbackUrl(req),
+      mode: 'single'
+    });
+
+    if (req.query.code !== undefined) {
+      // authenticated
+      org.authenticate(req.query, function(err) {
+        if (!err) {
+          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+            if (!err) {
+              res.render('index', {records: results.records});
+            }
+            else {
+              res.send(err.message);
+            }
+          });
+        }
+        else {
+          if (err.message.indexOf('invalid_grant') >= 0) {
+            res.redirect('/');
+          }
+          else {
+            res.send(err.message);
+          }
+        }
+      });
+    }
+    else {
+      res.redirect(org.getAuthUri());
+    }
+  }
+  else {
+    res.redirect('/setup');
+  }
+});
+
+app.get('/setup', function(req, res) {
+  if (isSetup()) {
+    res.redirect('/');
+  }
+  else {
+    var isRemote = (req.hostname.indexOf('localhost') != 0);
+    res.render('setup', { isRemote: isRemote, oauthCallbackUrl: oauthCallbackUrl(req) });
+  }
+});
+
+app.listen(process.env.PORT || 5000);
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+
+function loadEnv() {
+  require('gulp-env')({file: '.env', type: '.ini'});
+}
+
+
+// Start a server with livereload support
+gulp.task('server', function() {
+  loadEnv();
+
+  var livereload = require('gulp-livereload');
+  livereload.listen();
+
+  require('gulp-nodemon')({script: 'app.js', stdout: false, watch: ['.env', '*.*']})
+    .on('readable', function() {
+      // this waits until the actual server is up
+      this.stdout.on('data', function(chunk) {
+        if (/^Running/.test(chunk)) {
+          livereload.reload();
+        }
+        process.stdout.write(chunk);
+      });
+    })
+    .on('restart', function () {
+      loadEnv();
+    });
+});
+
+
+// Install the gulp-control Atom package and then run Atom
+gulp.task('atom', function(cb) {
+  var atomPaths = require('gulp-atom-downloader');
+  var proc = require('child_process');
+  var path = require('path');
+
+  atomPaths().then(function(atomPaths) {
+    // quietly install heroku-tools
+    proc.spawn(atomPaths.apm, ['install', '-q', 'heroku-tools']).on('close', function (code) {
+      if (code != 0) {
+        cb('Could not use apm to install heroku-tools.');
+      }
+      else {
+        proc.spawn(atomPaths.atom, ['./', 'README.md'], {env: {ATOM_PATH: atomPaths.base}}).on('close', function (code) {
+          if (code == 0) cb();
+          else cb('Atom shutdown with an error.');
+        });
+      }
+    });
+  });
+});
+
+// Default
+gulp.task('default', ['atom', 'server']);
